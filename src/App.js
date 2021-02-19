ea17c46ce5cd4b158ac26f4b48b9daa2e0193984
FABRICE KALVANDA <fabricekalvanda@gmail.com>
2021-02-19 01:36:36 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let greetingElement = (new Date()).getHours() < 12
+    ? <h3>Good Morning!</h3>
+    : <h3>Good Evening!</h3>;
+
+    let adjective = 'awesome';
+    let url = 'https://reactjs.org';
+  return (
+    <div className="App">
+      <header className="App-header">
+        {greetingElement}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This very {adjective}!!!
+        </p>
+
+        <a
+          className="App-link"
+          href= {url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
