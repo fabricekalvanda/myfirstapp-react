e9d80f7765962bd2e46d84c9636b8ddf78b247a7
FABRICE KALVANDA <fabricekalvanda@gmail.com>
2021-02-22 00:01:56 -0800

    pass props to components

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import './App.css';
 function App() {
     let adjective = 'awesome';
     let url = 'https://reactjs.org';
+
   return (
     <div className="App">
       <header className="App-header">
-        < Greeting />
+        < Greeting person={{name:"Farice", numberOfMessages: 11 }} 
+        name="Fabrice" numberOfMessages={89} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This very {adjective} for me!!!
