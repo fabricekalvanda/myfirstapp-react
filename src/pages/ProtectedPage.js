2b2450e1144a3df99c5d46dea158e53af2f74f1c
FABRICE KALVANDA <fabricekalvanda@gmail.com>
2021-03-23 16:42:33 -0700

    Navigate Programmatically

--- src/pages/ProtectedPage.js
+++ src/pages/ProtectedPage.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
 
 export const ProtectedPage = () => {
-    const isAuthed = true;
-    return isAuthed
-    ? (
+    const history = useHistory();
+
+    const isAuthed = false;
+
+    useEffect(() => {
+        if (!isAuthed) {
+            history.push('/');
+        }
+    });
+    return (
         <h1>Only authed users should be able to see this!</h1>
-    ): (
-        <Redirect to="/" />
     );
 }
